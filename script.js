4cd9017d619cb4cfcdd03796fa530609723b11ad
AlhasanDesigns <alhasaniscoding@outlook.com>
2023-01-05 20:34:27 -0500

    Fifth Commit

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const btn25 = document.getElementById('tip-25');
 const btn50 = document.getElementById('tip-50');
 const customTip = document.getElementById('custom-tip');
 const quantity = document.getElementById('quantity-number');
+const errorValue = document.getElementById('zero-value');
 const resetButton = document.getElementById('reset-button');
 
 const output1 = document.getElementById('tip-output');
@@ -34,7 +35,7 @@ function inputHandler(event) {
     if (+event.value > 0) {
       buttonFocusRemover();
       customBool = true;
-      return tipInput === event.value;
+      return tipInput = event.value / 100;
     }
   }
 
@@ -42,7 +43,11 @@ function inputHandler(event) {
     if (+event.value <= 0) {
       event.classList.remove('people-amount-input');
       event.classList.add('people-amount-input-false');
+      errorValue.classList.remove('error-false');
+      errorValue.classList.add('error-true');
     } else {
+      errorValue.classList.remove('error-true');
+      errorValue.classList.add('error-false');
       event.classList.remove('people-amount-input-false');
       event.classList.add('people-amount-input');
       return quantityInput = quantity.value;
@@ -99,9 +104,25 @@ function buttonInputHandler(event) {
 function setOutputs() {
   if (billInput > 0 && tipInput > 0 && quantityInput > 0) {
     let tip = (billInput * tipInput) / quantityInput;
-    output1.textContent = '$' + tip;
-    output2.textContent = '$' + tip + (billInput / quantityInput);
+    output1.textContent = '$' + tip.toFixed(2);
+    output2.textContent = '$' + (tip + (billInput / quantityInput)).toFixed(2);
   }
+}
+
+function resetInputs() {
+  buttonFocusRemover();
+  bill.value = '';
+  quantity.value = '';
+  customTip.value = '';
+  
+  billInput = 0;
+  tipInput = 0;
+  quantityInput = 0;
+  customBool = false; 
+
+  output1.textContent = '$0.00';
+  output2.textContent = '$0.00';
+
 }
 
 // Event Listeners that will add the values of the inputs into
@@ -131,6 +152,11 @@ function setOutputs() {
   btn15.addEventListener('click', () => setOutputs());
   btn25.addEventListener('click', () => setOutputs());
   btn50.addEventListener('click', () => setOutputs());
+
+
+  // Event Listener for resetting the calculator.
+
+  resetButton.addEventListener('click', () => resetInputs());
 
   
 
